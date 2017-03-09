d9b3d318128d08899d2ca369c1878d74173dd177
ioanaic <crisan.ioana27@gmail.com>
2017-03-09 11:27:33 +0200

    Add getters

--- src/Book.java
+++ src/Book.java
@@ -7,7 +7,7 @@ public class Book {
 
     private String bookName;
     private String bookTitle;
-    private String[] bookAuthors = null;
+    private String[][] bookAuthors = null;
     private boolean hasMultipleVolumes;
     private int volumeNumber;
     private Date startReadDate;
@@ -15,7 +15,7 @@ public class Book {
     private String bookGenre;
     private boolean isRead;
 
-    public Book(String bookName, String bookTitle, String[] bookAuthors, boolean hasMultipleVolumes, int volumeNumber, Date startReadDate, Date endReadDate, String bookGenre, boolean isRead) {
+    public Book(String bookName, String bookTitle, String[][] bookAuthors, boolean hasMultipleVolumes, int volumeNumber, Date startReadDate, Date endReadDate, String bookGenre, boolean isRead) {
         this.bookName = bookName;
         this.bookTitle = bookTitle;
         this.bookAuthors = bookAuthors;
@@ -23,6 +23,39 @@ public class Book {
         this.volumeNumber = volumeNumber;
         this.startReadDate = startReadDate;
         this.endReadDate = endReadDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getbookTitle() {
+        return bookTitle;
+    }
+
+    public String[][] getAuthorsList() {
+        return bookAuthors;
+    }
+
+    public boolean checkHasMultipleVolumes() {
+        return hasMultipleVolumes;
+    }
+    public int getVolumeNumber(){
+        return volumeNumber;
+    }
+    public Date getStartReadDate(){
+        return startReadDate;
+    }
+    public Date getEndReadDate(){
+        return endReadDate;
+    }
+
+    public String getBookGenre(){
+        return bookGenre;
+    }
+
+    public boolean checkIsRead(){
+        return isRead;
     }
 
 
