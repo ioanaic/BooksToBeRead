7996ac59aa9b95c216d474e0a71db4852838f291
ioanaic <crisan.ioana27@gmail.com>
2017-03-09 13:47:12 +0200

    Read books from console

--- src/Book.java
+++ src/Book.java
@@ -6,31 +6,27 @@ import java.util.Date;
 public class Book {
 
     private String bookName;
-    private String bookTitle;
+    private int numarulDeAutori;
     private String[][] bookAuthors = null;
-    private boolean hasMultipleVolumes;
+    // private boolean hasMultipleVolumes;
     private int volumeNumber;
     private Date startReadDate;
     private Date endReadDate;
     private String bookGenre;
     private boolean isRead;
 
-    public Book(String bookName, String bookTitle, String[][] bookAuthors, boolean hasMultipleVolumes, int volumeNumber, Date startReadDate, Date endReadDate, String bookGenre, boolean isRead) {
+    public Book(String bookName, int numarulDeAutori, String[][] bookAuthors, boolean hasMultipleVolumes, int volumeNumber, Date startReadDate, Date endReadDate, String bookGenre, boolean isRead) {
         this.bookName = bookName;
-        this.bookTitle = bookTitle;
         this.bookAuthors = bookAuthors;
-        this.hasMultipleVolumes = hasMultipleVolumes;
+        // this.hasMultipleVolumes = hasMultipleVolumes;
         this.volumeNumber = volumeNumber;
         this.startReadDate = startReadDate;
         this.endReadDate = endReadDate;
+        this.numarulDeAutori = numarulDeAutori;
     }
 
     public String getBookName() {
         return bookName;
-    }
-
-    public String getbookTitle() {
-        return bookTitle;
     }
 
     public String[][] getAuthorsList() {
@@ -38,24 +34,48 @@ public class Book {
     }
 
     public boolean checkHasMultipleVolumes() {
-        return hasMultipleVolumes;
+        if (volumeNumber > 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    public int getVolumeNumber(){
+
+    public int getVolumeNumber() {
         return volumeNumber;
     }
-    public Date getStartReadDate(){
+
+    public Date getStartReadDate() {
         return startReadDate;
     }
-    public Date getEndReadDate(){
+
+    public Date getEndReadDate() {
         return endReadDate;
     }
 
-    public String getBookGenre(){
+    public String getBookGenre() {
         return bookGenre;
     }
 
-    public boolean checkIsRead(){
+    public boolean checkIsRead() {
         return isRead;
+    }
+
+    public void readBooksFromConsole() {
+        int n = SkeletonJava.readIntConsole("Introduceti numarul de carti pe care doriti sa le cititi");
+        Book[] book = new Book[n];
+        for (int i = 0; i < n; i++) {
+            String bookName = SkeletonJava.readStringConsole("Introduceti numele cartii: ");
+            int numarulDeAutori = SkeletonJava.readIntConsole("Introduceti numarul de autori: ");
+            String[] bookAuthors = new String[numarulDeAutori];
+            for (int j = 0; j < numarulDeAutori; j++) {
+                bookAuthors[j] = SkeletonJava.readStringConsole("Introduceti numele autorulu: ");
+            }
+            int volumeNumber = SkeletonJava.readIntConsole("Introduceti numarul de volume: ");
+
+            this.startReadDate = startReadDate;
+            this.endReadDate = endReadDate;
+        }
     }
 
 
